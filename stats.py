8c5e19a20fc5470a9ad4bcba6c665f83e7920c2d
safon010 <safon010@umn.edu>
2023-04-05 18:40:25 -0500

    updating everything after charlie delete

--- stats.py
+++ stats.py
@@ -1,3 +1,4 @@
+# %%
 import csv
 import osmnx as ox
 import geopandas as gpd
@@ -5,34 +6,66 @@ import matplotlib.pyplot as plt
 import random
 import networkx as nx
 
-# open cin_stats.csv
 # %%
 # load stats from cin_stats.csv as a list of lists
 with open('cin_stats.csv', 'r') as f:
     reader = csv.reader(f)
     stats = list(reader)
+
 # %%
-# combine and replace all the distances into one list
+# load cincinnati_censustracts.json as a geodataframe
+zones = gpd.read_file('cincinnati_censustracts.json')
+# convert to graph objects
+graph = []
+for i in range(len(zones)):
+    print(i+1, "/" , str(len(zones)))
+    graph.append(ox.graph_from_polygon(zones.geometry[i], network_type='drive', retain_all=False, simplify=False, truncate_by_edge=True))
+G = nx.compose_all(graph)
+
+# %%
+# replace the strings in the first two columns with the corresponding graphs from cincinnati_censustracts.json
 for i in range(len(stats)):
-    stats[i] = stats[i][:2] + [float(x) for x in stats[i][2:]]
+    # find MOVEMENT_ID in first two columns
+    for j in range(2):
+        id = stats[i][j].split()[1]
+        # find the index of the graph with the same MOVEMENT_ID
+        for k in range(len(zones)):
+            if id == str(zones.MOVEMENT_ID[k]):
+                stats[i][j] = graph[k]
+                break
+            else:
+                continue
+
+ 
+# %%
+# combine and replace all the distances into one list, so len(stats[i]) = 3
+for i in range(len(stats)):
+    stats[i] = stats[i][:2] + [int(float(x[1:-1])) for x in stats[i][2:]]
+    stats[i] = [stats[i][0], stats[i][1], stats[i][2:]]
 # %%
 # add a column to stats with the average distance between the two zones
 for i in range(len(stats)):
-    stats[i].append(sum(stats[i][3])/len(stats[i][3]))
+    stats[i].append(sum(stats[i][2])/len(stats[i][2]))
+
 # %%
+# convert the first two columns of stats to GeoDataFrames
+zone1_gdf = []
+zone2_gdf = []
+for i in range(len(stats)):
+    zone1_gdf.append(ox.project_gdf(ox.graph_to_gdfs(stats[i][0], nodes=True, edges=False, fill_edge_geometry=True)[0]))
+    zone2_gdf.append(ox.project_gdf(ox.graph_to_gdfs(stats[i][1], nodes=True, edges=False, fill_edge_geometry=True)[0]))
 
 # add a column to stats with the ratio of the area of the first zone to the area of the second zone
 for i in range(len(stats)):
     # calculate the area of the first zone
-    zone1 = ox.graph_from_polygon(stats[i][0].geometry, network_type='drive', retain_all=True, simplify=False, truncate_by_edge=True)
-    zone1_area = ox.project_gdf(ox.graph_to_gdfs(zone1, nodes=False, edges=False, fill_edge_geometry=True)[1]).area[0]
+    zone1_area = zone1_gdf[i]['geometry'].area
     # calculate the area of the second zone
-    zone2 = ox.graph_from_polygon(stats[i][1].geometry, network_type='drive', retain_all=True, simplify=False, truncate_by_edge=True)
-    zone2_area = ox.project_gdf(ox.graph_to_gdfs(zone2, nodes=False, edges=False, fill_edge_geometry=True)[1]).area[0]
+    zone2_area = zone2_gdf[i]['geometry'].area
     # calculate the ratio of the first zone to the second zone
     stats[i].append(zone1_area/zone2_area)
-# %%
 
+
+# %%
 # add a column to stats with a list containing the land type (rural, suburban, urban) of the two zones
 for i in range(len(stats)):
     # convert zones to gdfs and extract the land type
@@ -56,4 +89,4 @@ for i in range(len(stats)):
 # %%
 # add a row on top of stats with the column names
 # ['zone1', 'zone2', '[d1', 'd2', ..., 'd1000]', 'avg_dist', 'ratio', 'land_type']
-stats = [['zone1', 'zone2', '[d1', ..., 'd1000]', 'avg_dist', 'ratio', '[land_type_1, land_type_2]']] + stats
+stats = [['zone1', 'zone2', '[d1, ..., d1000]', 'avg_dist', 'ratio', '[land_type_1, land_type_2]']] + stats
